7c2a50b94604e5f9503d21a191ff82daa74942ed
Jihun-Kim <anam4561@naver.com>
2023-06-21 22:09:40 +0900

    page set

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,17 +1,32 @@
-import { useState } from "react"; 
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
+import { Outlet, Route, Routes } from "react-router-dom";
 import "./App.css";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import Navbar from "./layout/Navbar";
+import Footer from "./layout/Footer";
+
+function Layout() {
+  return (
+    <div className="flex flex-col h-screen justify-between">
+      <Navbar />
+      <main className="mb-auto w-10/12 max-w-4xl mx-auto">
+        <Outlet />
+      </main>
+      <Footer />
+    </div>
+  );
+}
 
 function App() {
-  const [count, setCount] = useState(0);
-
-  return ( 
-    <div className="App">
-      <h1 className="text-3xl font-bold underline">
-        Hello World! 
-      </h1>
-    </div>
+  return (
+    <Routes>
+      <Route path="/" element={<Layout />}>
+        <Route index element={<LandingPage />} />
+        <Route path="/login" element={<LoginPage />} />
+        <Route path="/register" element={<RegisterPage />} />
+      </Route>
+    </Routes>
   );
 }
 
