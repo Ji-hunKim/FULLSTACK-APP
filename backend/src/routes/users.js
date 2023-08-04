f36bf51a4bd473a2b6d19e346a271991ffcbbc43
Jihun-Kim <anam4561@naver.com>
2023-08-04 23:48:46 +0900

    fix comparePassword

--- backend/src/routes/users.js
+++ backend/src/routes/users.js
@@ -18,12 +18,12 @@ router.post("/login", async (req, res, next) => {
   try {
     // 존재하는 유저인지 체크
     const user = await User.findOne({ email: req.body.email });
-    if (user) {
+    if (!user) {
       return res.status(400).send("Auth failed, email not found");
     }
-
+    
     // 비밀번호가 올바른 것인지 체크
-    const isMatch = await User.comparePassword(req.body.password);
+    const isMatch = await user.comparePassword(req.body.password);
     if (!isMatch) {
       return res.status(400).send("Wrong Password");
     }
