7aadb94ccd0675bac6fed4599bab28bc67e48e97
Jihun-Kim <anam4561@naver.com>
2023-08-31 21:30:17 +0900

    add components to LandingPage

--- frontend/src/pages/LandingPage/index.jsx
+++ frontend/src/pages/LandingPage/index.jsx
@@ -1,9 +1,81 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import CheckBox from "./Sections/CheckBox";
+import RadioBox from "./Sections/RadioBox";
+import SearchInput from "./Sections/SearchInput";
+import CardItem from "./Sections/CardItem";
+import axiosInstance from "../../utils/axios";
 
 const LandingPage = () => {
-  return (
-    <div>LandingPage</div>
-  )
-}
+  const limit = 4;
+  const [products, setproducts] = useState([]);
+  const [skip, setskip] = useState(0);
+  const [hasMore, sethasMore] = useState(false);
+  const [filters, setfilters] = useState({
+    continents: [],
+    price: [],
+  });
 
-export default LandingPage
+  useEffect(() => {
+    fetchProducts({skip, limit})
+  }, [])
+  
+
+  const fetchProducts = async (
+    skip,
+    limit,
+    loadMore = false,
+    filters = {},
+    searchTerm = ""
+  ) => {
+    const params = {
+      skip,
+      limit,
+      filters,
+      searchTerm,
+    };
+
+    try {
+      const response = await axiosInstance.get("/products", { params });
+      setproducts(response.data.products);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return (
+    <section>
+      <div className="text-center m-7">
+        <h2>Travel Packages Online Shopping Mall</h2>
+      </div>
+      {/* Filter */}
+      <div className="flex gap-3">
+        <div className="w-1/2">
+          <CheckBox />
+        </div>
+        <div className="w-1/2">
+          <RadioBox />
+        </div>
+      </div>
+      {/* Search */}
+      <div className="flex justify-end">
+        <SearchInput />
+      </div>
+      {/* Card */}
+      <div className="grid grid-cols-2 sm:grid-cols-4 gap-4">
+        {products.map((product) => (
+          <CardItem product={product} key={product._id} />
+        ))}
+      </div>
+      {/* Load More */}
+      (hasMore &&
+      <div className="flex justify-center mt-5">
+        <button className="px-4 py-2 mt-5 text-white bg-black round-md hover:bg-gray-500">
+          더 보기
+        </button>
+      </div>
+      )
+    </section>
+  );
+};
+
+export default LandingPage;
